4922f4f884676b47e0e4c32f1d4b4998af272fba
segers <jo.segers@gmail.com>
2018-08-25 18:17:50 +0200

    change to demo app

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <h1>GunVentory</h1>
+        <h1>GunVentory, demo app</h1>
         {_.map(guns, (gun, id) => (
           <GunInventoryCard
             key={id}
